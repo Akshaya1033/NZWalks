237934d3c2f75ff900849b7e46036b807de80263
akshaya.velpoor <akshaya.velpoor@TWINH-LAP-996>
2024-09-13 12:49:02 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/RegionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+           var regions=new List<Region>
+           {
+               new Region
+               {
+                   Id=Guid.NewGuid(),
+                   Name="Auckland Region",
+                   Code="AKL",
+                   RegionImageUrl=""
+               },
+               new Region
+               {
+                   Id=Guid.NewGuid(),
+                   Name="Auckland Region",
+                   Code="AKL",
+                   RegionImageUrl=""
+               }
+           }
+
+        
+        }
+    }
+}
